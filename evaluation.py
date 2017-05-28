96d3def8972cc873182bb445b88c85777433fbd7
TaesikGong <cathena913@gmail.com>
2017-05-28 22:59:18 +0900

    evaluation bug fixed

--- evaluation.py
+++ evaluation.py
@@ -145,18 +145,19 @@ if __name__ == '__main__':
             inp_vid, fut_vid = np.expand_dims(inp_vid, -1), np.expand_dims(fut_vid, -1)
 
             [fut_loss] = sess.run([net.fut_loss],feed_dict={net.input_frames: inp_vid,net.fut_frames: fut_vid})
-            print ("[step %d] loss: %f" % (step, fut_loss))
+
             min_loss = fut_loss
             sumloss+=min_loss
             o_vid = sess.run(net.fut_output, feed_dict={net.input_frames: inp_vid,net.fut_frames: fut_vid})
             o_vid = o_vid[0].reshape([opts.num_frames // 2, opts.image_size, opts.image_size])
             output_vid = np.concatenate((np.squeeze((x_batch * 255).astype(np.uint8))[0:opts.num_frames // 2], o_vid), axis=0)            
-            ResultData = []
-            if numIter<100:
+
+            if step < 100:
+                ResultData = []
                 ResultData.append(output_vid)
                 ResultData.append(np.squeeze((x_batch * 255).astype(np.uint8)).reshape((20,64,64)))
                 ResultData.append(fut_loss)
                 DataToVideo.MakeVideo(ResultData,step,Vdir_name)
 	        #DataToImg.MakeImage(output_vid,output)
             #DataToImg.MakeImage(np.squeeze((x_batch * 255).astype(np.uint8)).reshape((20,64,64)),true)
-        print("Average Loss = " + str(sumloss/np.float(numIter)))
+            print ("[step %d] loss: %f" % (step, fut_loss) + " Average Loss = " + str(sumloss/np.float(step + 1)))
